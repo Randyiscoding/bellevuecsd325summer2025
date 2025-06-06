00e9af87800b53798339d6afc1d55112ce7ebca7
Randyiscoding <reaston@my365.bellevue.edu>
2025-06-06 03:01:49 -0400

    Module 4 assignment created low temp plot

--- Module-4/sitka_high_low_rhe.py
+++ Module-4/sitka_high_low_rhe.py
@@ -9,17 +9,24 @@ with open(filename) as f:
     header_row = next(reader)
 
     # Get dates and high temperatures from this file.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         dates.append(current_date)
         high = int(row[5])
         highs.append(high)
+        low = int(row[6])
+        lows.append(low)
 
 # Plot the high temperatures.
 #plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+
+# Plot the low temperatures.
+#plt.style.use('seaborn')
+figs, ay = plt.subplots()
+ay.plot(dates, lows, c='blue')
 
 # Format plot.
 plt.title("Daily high temperatures - 2018", fontsize=24)
