4d7a7bea709a691f812f0260390482ef3160ee15
Randyiscoding <reaston@my365.bellevue.edu>
2025-06-16 20:49:02 -0400

    Module 4 assignment Completed

--- Module-4/sitka_high_low_rhe.py
+++ Module-4/sitka_high_low_rhe.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 from datetime import datetime
 
 from matplotlib import pyplot as plt
@@ -17,22 +18,39 @@ with open(filename) as f:
         highs.append(high)
         low = int(row[6])
         lows.append(low)
+    f.close()
 
-# Plot the high temperatures.
-#plt.style.use('seaborn')
-fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+while True:
+    while True:
+        # Use Instructions
+        hilow = input("Welcome to the Sitka Weather App!\n To View temperature Highs: Type 'H'\n To view temperature Lows: "
+                  "Type 'L'\n To Quit: Type 'Q'")
+        if hilow.upper() == 'L' or hilow.upper() == 'H' or hilow.upper() == 'Q':
+            break
+        else:
+            print("Incorrect input. Try Again")
 
-# Plot the low temperatures.
-#plt.style.use('seaborn')
-figs, ay = plt.subplots()
-ay.plot(dates, lows, c='blue')
+    if hilow.upper() == 'Q':
+        print("Thank you for using the Sitka Weather App. Have a Sunny Day. Goodbye!")
+        break
+        sys.exit(1)
+    elif hilow.upper() == 'H':
+        # Plot the High temperatures.
+        #plt.style.use('seaborn')
+        fig, ax = plt.subplots()
+        ax.plot(dates, highs, c='red')
+        plt.title("Daily high temperatures - 2018", fontsize=24)
+    else:
+        # Plot the low temperatures.
+        #plt.style.use('seaborn')
+        figs, ax = plt.subplots()
+        ax.plot(dates, lows, c='blue')
+        plt.title("Daily Low temperatures - 2018", fontsize=24)
 
-# Format plot.
-plt.title("Daily high temperatures - 2018", fontsize=24)
-plt.xlabel('', fontsize=16)
-fig.autofmt_xdate()
-plt.ylabel("Temperature (F)", fontsize=16)
-plt.tick_params(axis='both', which='major', labelsize=16)
+    # Format plot.
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel("Temperature (F)", fontsize=16)
+    plt.tick_params(axis='both', which='major', labelsize=16)
 
-plt.show()
+    plt.show()
